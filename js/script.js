a8cea10260ba7a07eddc002ba5eee8a772a02871
Kevin Fernando <kevin.fernando@supinfo.com>
2020-06-16 20:19:41 +0200

    Added two components

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
 const app = document.getElementById("app");
+
+Vue.component('button-counter', {
+    data: function () {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        incrementCounter: function () { this.counter++ }
+    },
+    template: `
+        <div class="button-counter-container">
+            <button class="btn btn-primary" v-on:click="incrementCounter">You clicked me {{counter}}</button>
+        </div>
+    `
+});
+
+Vue.component('hello-component', {
+    data: function() {
+        return {
+            msgComponent: "hello"
+        }
+    },
+    template: `
+        <div class="hello-component-container">
+            <p class="lead center">{{msgComponent}}</p>
+        </div>
+    `
+});
+
 const appVue = new Vue({
     el: app,
     data: {
@@ -24,29 +54,29 @@ const appVue = new Vue({
             this.loading = true;
 
             try {
-                if(this.users) { // resetting when the user is clicking again
+                if (this.users) { // resetting when the user is clicking again
                     this.users = null;
                 }
 
                 const data = await fetch("https://jsonplaceholder.typicode.com/users");
                 const jsonData = await data.json();
-    
+
                 this.loading = false;
                 this.users = jsonData;
             }
-            
+
             catch (err) {
                 console.log("error while retrieving data.");
                 console.log(err);
             }
         },
-        navToGoogle: function() {
+        navToGoogle: function () {
             window.open(this.googleLink, "_blank");
         },
-        authUser: function(e) {
+        authUser: function (e) {
             e.preventDefault();
 
-            if(this.userPassword !== "1234") {
+            if (this.userPassword !== "1234") {
                 console.log("if");
 
                 this.authErrors.error = true;
@@ -56,7 +86,7 @@ const appVue = new Vue({
             else {
                 this.authErrors = false;
                 this.authErrors.msg = "";
-                
+
                 alert(`Welcome ${this.userEmail} !`);
             }
         }
